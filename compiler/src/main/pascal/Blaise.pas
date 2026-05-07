d2209afd0ef931d0b77135573a436854f6592c64
Graeme Geldenhuys <graemeg@gmail.com>
2026-05-07 08:24:50 +0100

    release: v0.5.0
    Fixpoint achieved on the full multi-file source (105865 lines of QBE IR).
    Ships indexed-property getter codegen and string-field char byte-load, fixing
    the self-hosting crash introduced by overloading support in v0.4.0.

--- compiler/src/main/pascal/Blaise.pas
+++ compiler/src/main/pascal/Blaise.pas
@@ -27,7 +27,7 @@ uses
   uLexer, uParser, uAST, uSemantic, uCodeGenQBE, uUnitLoader, uDebugOPDF;
 
 const
-  Version = '0.5.0-dev';
+  Version = '0.5.0';
   CompilerName = 'Blaise';
 
 procedure PrintUsage;
